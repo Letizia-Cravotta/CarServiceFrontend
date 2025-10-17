3c2bd73c1fb94e05958c61090145e03fda3852b5
Leti-NTT <letizia.cravotta@emeal.nttdata.com>
2025-10-17 12:49:55 +0200

    DHBW2025-75: Implement error message extractor and display of error messages for user

--- src/app/car-editor/car-editor.ts
+++ src/app/car-editor/car-editor.ts
@@ -1,6 +1,6 @@
-import {Component, Input, Output, EventEmitter, inject, OnChanges, SimpleChanges} from '@angular/core';
+import {Component, Input, Output, EventEmitter, inject, OnChanges, SimpleChanges, signal} from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import {ApiService, Car} from '../api';
+import {ApiService, Car, extractErrorMessage} from '../api';
 import {CommonModule} from '@angular/common';
 
 @Component({
@@ -17,12 +17,15 @@ export class CarEditor implements OnChanges {
 
   private apiService = inject(ApiService);
   // Create a local copy for editing to avoid changing the original object directly.
-  editableCar: Car = { brand: '', color: '', numberOfWheels: 4 };
+  editableCar: Car = {brand: '', color: '', numberOfWheels: 4};
+
+  error = signal<string | null>(null);
 
   ngOnChanges(changes: SimpleChanges): void {
     // When the input car changes, clone it to `editableCar`.
     if (changes['car'] && this.car) {
-      this.editableCar = { ...this.car };
+      this.editableCar = {...this.car};
+      this.error.set(null)
     }
   }
 
@@ -32,14 +35,20 @@ export class CarEditor implements OnChanges {
         next: () => {
           console.log('Car updated successfully');
           this.carUpdated.emit(); // Notify parent that update was successful.
+          this.error.set(null)
           this.close.emit(); // Close the modal.
         },
-        error: (err) => console.error('Error updating car:', err)
+        error: (err) => {
+          const msg = extractErrorMessage(err);
+          this.error.set(msg);
+          console.error('Error updating car:', err)
+        }
       });
     }
   }
 
   closeModal(): void {
+    this.error.set(null);
     this.close.emit();
   }
 }
